b6ebd84df71118999b16d3f75dc5d9e6edcaeaf8
ColtonHagan <coltonmhagan@gmail.com>
2021-10-27 19:53:01 -0700

    added logic for import/exporting via string

--- app/src/main/java/com/pagewisegroup/pagewise/StudentViewActivity.kt
+++ app/src/main/java/com/pagewisegroup/pagewise/StudentViewActivity.kt
@@ -2,10 +2,20 @@ package com.pagewisegroup.pagewise
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import com.google.android.material.textfield.TextInputLayout
 
 class StudentViewActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_studentview)
+    }
+
+    /* creates assignment from id */
+    fun createAssignment(id: String) {
+        if(id.isBlank()) return
+        val assignment = Assignment.fromId(id)
+        Log.d("Assignment", assignment.toString())
     }
 }
