1278c9b2d55a60589756c485b02ed5d7543f5b5e
kurtzhk <kurtzhk@wwu.edu>
2021-11-26 19:20:05 -0800

    student view work

--- app/src/main/java/com/pagewisegroup/pagewise/StudentViewActivity.kt
+++ app/src/main/java/com/pagewisegroup/pagewise/StudentViewActivity.kt
@@ -3,15 +3,45 @@ package com.pagewisegroup.pagewise
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
 import android.util.Log
-import android.widget.EditText
+import android.view.View
+import android.widget.*
 import com.google.android.material.textfield.TextInputLayout
 
 class StudentViewActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_studentview)
+
+        //student taskbar actions are initialized here
+        val studentActionsStrings = resources.getStringArray(R.array.StudentActions)
+
+        val spinner = findViewById<Spinner>(R.id.student_spinner)
+        if (spinner != null) {
+            val adapter =
+                ArrayAdapter(this, android.R.layout.simple_spinner_item, studentActionsStrings)
+            spinner.adapter = adapter
+        }
+
+        spinner.onItemSelectedListener = object :
+            AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(
+                parent: AdapterView<*>,
+                view: View,
+                position: Int,
+                id: Long
+            ) {
+                Toast.makeText(
+                    this@StudentViewActivity,
+                    studentActionsStrings[position],
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>) {
+                // write code to perform some action
+            }
+        }
 
         //all this nonsense is just for my testing purposes
         val btn = findViewById<Button>(R.id.atest_button)
